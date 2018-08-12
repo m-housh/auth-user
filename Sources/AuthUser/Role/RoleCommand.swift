adad551cdba58c5137b8ff83797d5208045ab26e
Michael Housh <mhoush@houshhomeenergy.com>
2018-08-12 11:57:55 -0400

    Working on role-command

--- Sources/AuthUser/Role/RoleCommand.swift
+++ Sources/AuthUser/Role/RoleCommand.swift
@@ -27,14 +27,25 @@ public struct RoleCommand<D>: Command where D: QuerySupporting {
     
     public func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
         let name = try context.argument("name")
-        let dbid = try RoleType.requireDefaultDatabase()
+        //let dbid = try RoleType.requireDefaultDatabase()
+        let client = try context.container.make(Client.self)
+        let url = URL(string: "/user/role/findOrCreate/\(name)")!
         
+        return client.get(url).flatMap { resp in
+            return try resp.content.decode(RoleType.self).flatMap { role in
+                context.console.print()
+                context.console.print("Created role: \(role.name), \(role.id)")
+                return .done(on: context.container)
+            }
+        }
+        
+        /*
         return context.container.withPooledConnection(to: dbid, closure: { conn in
             return try RoleType.findOrCreate(name, on: conn).flatMap { role in
                 context.console.print("Created role: \(role.name)")
                 _ = role.save(on: conn)
                 return .done(on: context.container)
             }
-        }) 
+        }) */
     }
 }
